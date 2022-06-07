3fc3ed63617bf34b5617ea4ad8cefe6171c20fdd
Jorge Ariztegui <jorgeariztegui8@gmail.com>
2022-06-07 00:09:53 -0300

    se termino etapa Ver.

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div className='row m-0 p-0'>
         <div className='col-3 colum-izq'>
-            <h2 className='text-white text-center mt-4 mb-5 fw-bold'>CRM - Clientes</h2>
+            <h2 className='text-white text-center mt-4 mb-5 fw-bold'>ADM - Clientes</h2>
             <nav className='d-flex flex-column fs-5'>
                 <NavLink className='text-white text-decoration-none mb-2' to='/clientes'
                 >Clientes</NavLink>
--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner';
 
 const VerCliente = () => {
 
+  const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(true)
+
+  const {nombre, email, telefono, empresa, notas} = cliente;
+
+  useEffect(()=>{
+    const consultarApi = async ()=>{
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+      setCliente(resultado);
+
+      } catch (error) {
+        console.log(error);
+      }
+      
+      setTimeout(()=>{
+        setCargando(!cargando)
+      },2000)
+    }
+    consultarApi();
+  }, [])
+
     const {id} = useParams()
-    console.log(id);
 
   return (
-    <div>VerCliente</div>
+    cargando ? <Spinner />
+      : 
+        Object.keys(cliente).length === 0 ? <p className='fs-4'>No hay Resultados</p> : (
+      <div className='bg-white w-75 m-auto text-left p-3 rounded-2'>
+        <h2 className='fw-bold title-nuevo-cliente'>Cliente - <span className='text-uppercase'>{nombre}</span></h2>
+        <div>
+          {cliente.nombre && (
+            <p className='text-uppercase fw-semibold fs-5 mt-5'>email: <span className='text-lowercase fw-normal'>{email}</span></p>
+          )}
+          {cliente.telefono && (
+            <p className='text-uppercase fw-semibold fs-5 mt-2'>telefono: <span className='text-lowercase fw-normal'>{telefono}</span></p>
+          )}
+          {cliente.empresa && (
+            <p className='text-uppercase fw-semibold fs-5 mt-2'>empresa: <span className='text-lowercase fw-normal'>{empresa}</span></p>
+          )}
+          {cliente.notas && (
+            <p className='text-uppercase fw-semibold fs-5 mt-2'>notas: <span className='text-lowercase fw-normal'>{notas}</span></p>
+          )}
+        </div>
+    </div>
+    )
+
+    
   )
 }
 
